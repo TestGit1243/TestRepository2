71e9ed8203925389c99cb46a0cc19b726da855a8
Taimoor Ahmed <testabccompany20@gmail,com>
2023-11-27 17:15:52 +0500

    test

--- TestGithubRepository/Controllers/HomeController.cs
+++ TestGithubRepository/Controllers/HomeController.cs
@@ -13,5 +13,10 @@ namespace TestGithubRepository.Controllers
             return View();
         }
 
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
     }
 }
